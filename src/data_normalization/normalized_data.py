66b3f911c1b826164ff9713b5d9233a5a07184cd
zolizoli <zoltan.varju@gmail.com>
2019-11-05 17:44:14 +0100

    add tasks for processing the raw data

---
+++ src/data_normalization/normalized_data.py
@@ -0,0 +1,6 @@
+# 1 dedupe songs
+# 2 dedupe persons
+# 3 link persons to performers, authors and composers
+# 4 generate a graph which links ppl through songs
+# 5 make a language model for songs
+# 6 generate a graph which links songs through ppl
